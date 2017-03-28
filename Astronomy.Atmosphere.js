43740cc3e3de34b41d16e8d5aaadd144928471cf
lhttjdr <lhttjdr@gmail.com>
2017-03-28 21:00:08 +0900

    correct misspelled word

--- Astronomy.Atmosphere.js
+++ Astronomy.Atmosphere.js
@@ -1,10 +1,12 @@
+/*
+ * 蒙气修正
+ * h：真高度（高度角）； ho：视高度（地平纬度）
+ */
 (function() {
   var Atomsphere = Calendar.createNS("Calendar.Atomsphere");
-  //大气折射,h是真高度
   Atomsphere.correctAltitude = function(h) {
     return 0.0002967 / Math.tan(h + 0.003138 / (h + 0.08919));
   };
-  //大气折射,ho是视高度
   Atomsphere.correctApparentAltitude = function(ho) {
     return -0.0002909 / Math.tan(ho + 0.002227 / (ho + 0.07679));
   }
